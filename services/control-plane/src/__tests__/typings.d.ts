0dc897aa5a9cae55a7812ac4101c7744ea205fe7
Epistemophile <brainstein@tutanota.com>
2025-10-23 19:58:17 +0200

    feat: add urlscan artifact paths and handlers

---
+++ services/control-plane/src/__tests__/typings.d.ts
@@ -0,0 +1,16 @@
+declare module 'url-expand' {
+  export default function expand(url: string): Promise<string>;
+}
+
+declare module 'bottleneck' {
+  export default class Bottleneck {
+    constructor(...args: any[]);
+    schedule<T>(fn: (...args: any[]) => Promise<T> | T, ...args: any[]): Promise<T>;
+    on(event: string, handler: (...args: any[]) => void): void;
+    currentReservoir(): Promise<number | null>;
+  }
+}
+
+declare module 'confusables' {
+  export default function removeConfusables(value: string): string;
+}
